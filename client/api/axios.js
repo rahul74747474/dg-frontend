da7d2435d19bbe18dae7827755e7139861ba23a4
rahul74747474 <ghibli141525@gmail.com>
2026-04-03 00:50:31 +0530

    Update axios.js

--- client/api/axios.js
+++ client/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://dg-backend-nm4d.onrender.com/api",
+  //baseURL: "http://localhost:5000/api",
+   baseURL: "https://dg-backend-nm4d.onrender.com/api",
   withCredentials: true, // 🔥 ADD THIS
 });
 
